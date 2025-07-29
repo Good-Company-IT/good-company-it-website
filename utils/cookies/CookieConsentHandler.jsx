4808c58763a911e85a0b25f33e5517c3fbb5b59b
Andrés Poveda <81783473+apovedq@users.noreply.github.com>
2025-07-29 07:55:51 -0500

    refactor: remove clarity from cookie consent

--- utils/cookies/CookieConsentHandler.jsx
+++ utils/cookies/CookieConsentHandler.jsx
@@ -37,26 +37,6 @@ const updateConsentState = (hasConsent) => {
   }
 };
 
-const ClarityScript = ({ isActive }) => {
-  if (!isActive) return null;
-  
-  return (
-    <Script
-      id="microsoft-clarity"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "pm3krnpgtr");
-        `
-      }}
-    />
-  );
-};
-
 const CookieBanner = ({ onAccept, onDecline, translations }) => {
   return (
     <div className="fixed mb:max-w-[300px] bottom-0 border-t mb:border-r border-orange-400 rounded-t-xl rounded-r-xl p-6 backdrop-filter backdrop-blur z-50" style={styles}>
@@ -169,7 +149,6 @@ export default function CookieConsentHandler({ translations }) {
           </motion.div>
         )}
       </AnimatePresence>
-      <ClarityScript isActive={hasConsent} />
     </>
   );
 }
